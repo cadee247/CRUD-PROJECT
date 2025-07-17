0fd3a468c1ba8b0d080193262652363da2228609
cadee247 <rousseaucadee@gmail.com>
2025-07-17 17:42:00 +0200

    Update urls.py

--- CRUD/project/project/urls.py
+++ CRUD/project/project/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls')),
+    path('', lambda request: HttpResponse("🚀 Hello Cadee! Your app is officially live.")),
 ]
